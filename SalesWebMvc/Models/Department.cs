e5cc389559cf619984df08642f5ff03675e921cf
Erick-Rick <henrique.eryck@gmail.com>
2021-10-24 16:55:01 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,30 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        //Associação de departamento para selles (1 pode possuir varios vendedores)
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name) // não é incluido coleções
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller) 
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) 
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
+
+
 }
