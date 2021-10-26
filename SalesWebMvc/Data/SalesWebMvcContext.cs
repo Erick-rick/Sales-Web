17b96912a275962944fba8c4f401c682bc6b9d10
Erick-Rick <henrique.eryck@gmail.com>
2021-10-25 23:15:14 -0300

    Seeding Services

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -16,7 +16,7 @@ namespace SalesWebMvc.Data
         //Add Dbset dos models
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller{ get; set; }
-        public DbSet<SalesRecord> SalesRecords { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
 
     }
 }
