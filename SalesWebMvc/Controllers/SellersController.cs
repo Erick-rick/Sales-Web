cb4517fade41cdb9735a50a4e005918322fce1ff
Erick-Rick <henrique.eryck@gmail.com>
2021-11-04 22:10:33 -0300

    Details sellers

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -48,6 +48,7 @@ namespace SalesWebMvc.Controllers
             }
 
             var obj = _sellerService.FindById(id.Value);
+            
             if(obj == null)
             {
                 return NotFound();
@@ -61,6 +62,24 @@ namespace SalesWebMvc.Controllers
         {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+
         }
     }
 }
